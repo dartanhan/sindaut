308957eed6f9fdfdc52934b4823421846fd2ba13
Dartanhan <dartanhan.silva@extreme.digital>
2023-10-14 18:39:09 -0300

    acerto caminho do arquivos

--- resources/views/site/popular-post.blade.php
+++ resources/views/site/popular-post.blade.php
@@ -3,39 +3,39 @@
     <ul class="spost_nav">
         <li>
             <div class="media wow fadeInDown">
-                <a href="{{URL::asset('../../storage/posts/files/658767de4359d6772fbc14b6291c2d7a.pdf')}}" target="_blank" class="media-left">
+                <a href="{{URL::asset('storage/posts/files/658767de4359d6772fbc14b6291c2d7a.pdf')}}" target="_blank" class="media-left">
                     <img alt="" src="{{URL::asset('images/jornal.png')}}"> </a>
                 <div class="media-body">
-                    <a href="{{URL::asset('../../storage/posts/files/658767de4359d6772fbc14b6291c2d7a.pdf')}}" target="_blank"  class="catg_title"> JORNAL DO SINDAUT 47</a>
+                    <a href="{{URL::asset('storage/posts/files/658767de4359d6772fbc14b6291c2d7a.pdf')}}" target="_blank"  class="catg_title"> JORNAL DO SINDAUT 47</a>
                 </div>
             </div>
         </li>
         <div class="media wow fadeInDown">
-            <a href="{{URL::asset('../../storage/posts/files/feaf472531f672a2cd0e7ef0024d9922.pdf')}}" target="_blank" class="media-left">
+            <a href="{{URL::asset('storage/posts/files/feaf472531f672a2cd0e7ef0024d9922.pdf')}}" target="_blank" class="media-left">
                 <img alt="" src="{{URL::asset('images/jornal.png')}}"> </a>
             <div class="media-body">
-                <a href="{{URL::asset('../../storage/posts/files/feaf472531f672a2cd0e7ef0024d9922.pdf')}}" target="_blank"  class="catg_title"> JORNAL DO SINDAUT 46</a>
+                <a href="{{URL::asset('storage/posts/files/feaf472531f672a2cd0e7ef0024d9922.pdf')}}" target="_blank"  class="catg_title"> JORNAL DO SINDAUT 46</a>
             </div>
         </div>
         <div class="media wow fadeInDown">
-            <a href="{{URL::asset('../../storage/posts/files/7847755032cd1cdb1bfe9f870dd1729d.pdf')}}" target="_blank" class="media-left">
+            <a href="{{URL::asset('storage/posts/files/7847755032cd1cdb1bfe9f870dd1729d.pdf')}}" target="_blank" class="media-left">
                 <img alt="" src="{{URL::asset('images/jornal.png')}}"> </a>
             <div class="media-body">
-                <a href="{{URL::asset('../../storage/posts/files/7847755032cd1cdb1bfe9f870dd1729d.pdf')}}" target="_blank"  class="catg_title"> JORNAL DO SINDAUT 45</a>
+                <a href="{{URL::asset('storage/posts/files/7847755032cd1cdb1bfe9f870dd1729d.pdf')}}" target="_blank"  class="catg_title"> JORNAL DO SINDAUT 45</a>
             </div>
         </div>
         <div class="media wow fadeInDown">
-            <a href="{{URL::asset('../../storage/posts/files/406e596a56d9c01c5faf9c408e25280c.pdf')}}" target="_blank" class="media-left">
+            <a href="{{URL::asset('storage/posts/files/406e596a56d9c01c5faf9c408e25280c.pdf')}}" target="_blank" class="media-left">
                 <img alt="" src="{{URL::asset('images/jornal.png')}}"> </a>
             <div class="media-body">
-                <a href="{{URL::asset('../../storage/posts/files/406e596a56d9c01c5faf9c408e25280c.pdf')}}" target="_blank"  class="catg_title"> JORNAL DO SINDAUT 44</a>
+                <a href="{{URL::asset('storage/posts/files/406e596a56d9c01c5faf9c408e25280c.pdf')}}" target="_blank"  class="catg_title"> JORNAL DO SINDAUT 44</a>
             </div>
         </div>
         <div class="media wow fadeInDown">
-            <a href="{{URL::asset('../../storage/posts/files/41f2fbf1d20a958513e3b6b8c61d3e13.pdf')}}" target="_blank" class="media-left">
+            <a href="{{URL::asset('storage/posts/files/41f2fbf1d20a958513e3b6b8c61d3e13.pdf')}}" target="_blank" class="media-left">
                 <img alt="" src="{{URL::asset('images/jornal.png')}}"> </a>
             <div class="media-body">
-                <a href="{{URL::asset('../../storage/posts/files/41f2fbf1d20a958513e3b6b8c61d3e13.pdf')}}" target="_blank"  class="catg_title"> JORNAL DO SINDAUT 43</a>
+                <a href="{{URL::asset('storage/posts/files/41f2fbf1d20a958513e3b6b8c61d3e13.pdf')}}" target="_blank"  class="catg_title"> JORNAL DO SINDAUT 43</a>
             </div>
         </div>
     </ul>
