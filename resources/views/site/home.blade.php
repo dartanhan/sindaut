7bd03b194875cc3fe03e09a507cc8ac15f72f4d4
Dartanhan <dartanhan.lima@gmail.com>
2026-06-25 14:33:24 -0300

    feat: implement SiteController and home page layout with dynamic news grid and slider components

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -8,36 +8,106 @@
 
 @section('content')
 
+<style>
+    .news-square-box {
+        display: block; 
+        position: relative; 
+        height: 219px; 
+        overflow: hidden; 
+        border-radius: 4px; 
+        box-shadow: 0 2px 5px rgba(0,0,0,0.15);
+    }
+    .news-square-box:hover .square-img {
+        transform: scale(1.05);
+    }
+    .square-img {
+        width: 100%; 
+        height: 100%; 
+        object-fit: cover; 
+        object-position: center; 
+        transition: transform 0.3s;
+    }
+    @media (max-width: 991px) {
+        .news-square-box {
+            height: 195px;
+        }
+    }
+    @media (max-width: 767px) {
+        .news-square-box {
+            height: 180px;
+        }
+    }
+</style>
+
 <section id="contentSection">
+    <!-- Top Row: Slider and 4 Squares Grid -->
+    <div class="row" style="margin-bottom: 25px;">
+        <div class="col-lg-7 col-md-7 col-sm-12">
+            @include('site/sliders', ['noticias' => $noticias->slice(0, 5)])
+        </div>
+        <div class="col-lg-5 col-md-5 col-sm-12">
+            <div class="row" style="margin-left: -5px; margin-right: -5px;">
+                @foreach($noticias->slice(5, 4) as $key => $noticia)
+                    @php 
+                        $hasImage = false;
+                        $imagePath = '';
+                        foreach($noticia['imagens'] as $imagem) {
+                            if(!empty($imagem) && strlen($imagem->path) > 0) {
+                                $hasImage = true;
+                                $imagePath = $imagem->path;
+                                break;
+                            }
+                        }
+                    @endphp
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6" style="padding-left: 5px; padding-right: 5px; margin-bottom: 10px;">
+                        <a href="{{ route('site.detalhe-noticia', $noticia->id) }}" class="news-square-box">
+                            @if($hasImage)
+                                <img src="{{ URL::asset('storage/posts/files/'.$imagePath) }}" alt="{{ $noticia->titulo }}" class="square-img">
+                            @else
+                                <img src="{{ URL::asset('images/volume.png') }}" alt="{{ $noticia->titulo }}" class="square-img">
+                            @endif
+                            <div style="position: absolute; bottom: 0; left: 0; right: 0; background: linear-gradient(transparent, rgba(0,0,0,0.85)); padding: 12px 10px 8px; color: #fff;">
+                                <h3 style="margin: 0 0 5px; font-size: 11px; font-weight: bold; line-height: 1.3; height: 28px; overflow: hidden; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;">
+                                    {{ $noticia->titulo }}
+                                </h3>
+                                <span style="font-size: 9px; opacity: 0.8; display: block;">
+                                    <i class="fa fa-calendar" style="margin-right: 3px;"></i> {{ $noticia->created_at }}
+                                </span>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+
+    <!-- Bottom Row: Featured Post and Sidebar -->
     <div class="row">
         <div class="col-lg-8 col-md-8 col-sm-6 left-column">
             <div class="row">
-                @include('site/sliders', ['variavel' => '$valor'])
-            </div>
-            <div class="row">
-                @if(count($noticias) > 0)
+                @if(count($noticias) > 9)
+                    @php $featuredNoticia = $noticias[9]; @endphp
                     <div class="col-lg-12 col-md-8 col-sm-8">
                         <div class="left_content">
                             <div class="single_page">
-
-                                <h1>{{$noticias[0]->titulo}}</h1>
+                                <h1>{{$featuredNoticia->titulo}}</h1>
                                 <div class="post_commentbox">
                                     @php
-                                        $criado = $noticias[0]->getRawOriginal('created_at');
-                                        $atualizado = $noticias[0]->getRawOriginal('updated_at');
+                                        $criado = $featuredNoticia->getRawOriginal('created_at');
+                                        $atualizado = $featuredNoticia->getRawOriginal('updated_at');
                                         $mostraAtualizado = $criado && $atualizado && (strtotime($atualizado) - strtotime($criado) > 60);
                                         
-                                        $textoLimpo = strip_tags($noticias[0]->conteudo);
+                                        $textoLimpo = strip_tags($featuredNoticia->conteudo);
                                         $palavras = count(explode(' ', preg_replace('/\s+/', ' ', trim($textoLimpo))));
                                         $tempoLeitura = max(1, ceil($palavras / 200));
                                     @endphp
                                     <span>
-                                        <i class="fa fa-calendar"></i> Publicado em: {{$noticias[0]->created_at}}
+                                        <i class="fa fa-calendar"></i> Publicado em: {{$featuredNoticia->created_at}}
                                     </span>
                                     @if($mostraAtualizado)
                                         &nbsp;&nbsp;
                                         <span>
-                                            <i class="fa fa-history"></i> Atualizado em: {{$noticias[0]->updated_at}}
+                                            <i class="fa fa-history"></i> Atualizado em: {{$featuredNoticia->updated_at}}
                                         </span>
                                     @endif
                                     &nbsp;&nbsp;
@@ -46,7 +116,7 @@
                                     </span>
                                 </div>
                                 <div class="single_page_content">
-                                    {!! str_replace("../../", "", $noticias[0]->conteudo) !!}
+                                    {!! str_replace("../../", "", $featuredNoticia->conteudo) !!}
                                 </div>
                             </div>
                         </div>
@@ -55,8 +125,8 @@
             </div>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-4 right-column">
-            @if(count($noticias) > 0)
-                @include('site/ultimas-noticias', ['variavel' => '$valor'])
+            @if(count($noticias) > 10)
+                @include('site/ultimas-noticias', ['noticias' => $noticias->slice(10, 5)])
             @endif
             @include('site/popular-post', ['variavel' => '$valor'])
         </div>
